da850fa292fb3cf041ddbd2b7736c898af617d6f
Daazed J McFarland <daazedjmcfarland@gmail.com>
2026-01-03 04:57:59 -0800

    refine

--- lib/watcher.dart
+++ lib/watcher.dart
@@ -3,26 +3,31 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
 
-// {
-//   "$Bundles": {
-//     "System.Runtime": "1.0.0.0",
-//     "VRage": "2.0.2.51"
-//   },
-//   "$Type": "VRage:Keen.VRage.Library.Filesystem.StorageManagers.ContainerInfo`1<VRage:Keen.VRage.Core.Worlds.WorldSessionSaveGameMetaData>",
-//   "$Value": {
-//     "Meta": {
-//       "BaseMetadata": null,
-//       "DisplayName": "again",
-//       "Description": "",
-//       "GameVersion": 2000002,
-//       "GameBuildNumber": 51,
-//       "SaveGameCreationTimeInTicks": 639029565634567089,
-//       "PCU": 200284,
-//       "UsedDebugMenu": false
-//     },
-//     "AdditionalData": {}
-//   }
-// }
+//TODO: detect child save and then overwrite parent with that child
+// i.e, you load save_3, when you save it should not be save_3_2, it should move current parent to save_2 and save as the new parent save.
+// I'm thinking I'll do what I did with that mermaid port and hash the save, keep a map in the root of the save dir to track managed saves
+class SaveMeta {
+  final int slot;
+  final String parent;
+  const SaveMeta({required this.slot, required this.parent});
+
+  factory SaveMeta.fromJson(json) {
+    return SaveMeta(slot: json['slot'], parent: json['parent']);
+  }
+
+  factory SaveMeta.fromDirectory(Directory dir) {
+    final metaFile =
+        (dir.listSync().firstWhere((e) => e.path.contains('.autobackup'))
+                as File)
+            .readAsStringSync();
+    return SaveMeta.fromJson(jsonDecode(metaFile));
+  }
+
+  Map<String, dynamic> toJson() {
+    return {"slot": slot, "parent": parent};
+  }
+}
+
 class ContainerInfo {
   final ContainerBundles bundles;
   final String type;
@@ -40,6 +45,17 @@ class ContainerInfo {
       value: ContainerValue.fromJson(json[r'$Value']),
     );
   }
+
+  factory ContainerInfo.fromDirectory(Directory dir) {
+    final json = jsonDecode(getContainerInfo(dir).readAsStringSync());
+    return ContainerInfo.fromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => {
+    r'$Bundles': bundles.toJson(),
+    r'$Type': type,
+    r'$Value': value.toJson(),
+  };
 }
 
 class ContainerBundles {
@@ -53,6 +69,10 @@ class ContainerBundles {
       vRage: json['VRage'] as String,
     );
   }
+  Map<String, dynamic> toJson() => {
+    'System.Runtime': systemRuntime,
+    'VRage': vRage,
+  };
 }
 
 class ContainerValue {
@@ -69,18 +89,22 @@ class ContainerValue {
       containerMeta: ContainerMeta.fromJson(json['Meta']),
     );
   }
+  Map<String, dynamic> toJson() => {
+    'Meta': containerMeta.toJson(),
+    'AdditionalData': additonalData,
+  };
 }
 
 class ContainerMeta {
   final String? baseMetadata;
-  final String displayName;
+  String displayName;
   final String description;
   final int gameVersion;
   final int gameBuildNumber;
   final int saveCreationTimeInTicks;
   final int pcu;
   final bool usedDebugMenu;
-  const ContainerMeta({
+  ContainerMeta({
     this.baseMetadata,
     required this.displayName,
     required this.description,
@@ -103,13 +127,31 @@ class ContainerMeta {
       usedDebugMenu: json['UsedDebugMenu'] as bool,
     );
   }
+
+  Map<String, dynamic> toJson() => {
+    'BaseMetadata': baseMetadata,
+    'DisplayName': displayName,
+    'Description': description,
+    "GameVersion": gameVersion,
+    "GameBuildNumber": gameBuildNumber,
+    "SaveGameCreationTimeInTicks": saveCreationTimeInTicks,
+    "PCU": pcu,
+    "UsedDebugMenu": usedDebugMenu,
+  };
 }
 
 const int MAX_SAVES = 4;
 
-void copyDirectory(Directory src, Directory dst) {
+void copyDirectory({
+  required Directory src,
+  required Directory dst,
+  required SaveMeta meta,
+}) {
   Process.runSync('robocopy', [src.path, dst.path, '/E', '/B']);
-  File("${dst.path}/.autobackup").createSync();
+  final metaLoc = File("${dst.path}/.autobackup");
+  metaLoc.createSync();
+  final pp = JsonEncoder.withIndent(' ').convert(meta.toJson());
+  metaLoc.writeAsStringSync(pp);
 }
 
 bool isAuto(Directory saveDir) {
@@ -120,30 +162,69 @@ bool isAuto(Directory saveDir) {
   return isBackup;
 }
 
-void backupAndRenameSavesDirs(
-  List<FileSystemEntity> saveContentDirs,
-  Directory saveDir,
-  String saveLoc,
-  String saveName,
-  bool isBackup,
-) {
-  final existingSaves = saveContentDirs.where((e) => e.path.contains(saveName));
+File getContainerInfo(Directory dir) {
+  final cFile = dir.listSync().firstWhere(
+    (e) => e.path.contains('.container-info'),
+  );
+  return cFile as File;
+}
 
-  if (saveDir.parent.path == saveLoc && !isBackup) {
-    final saveList = existingSaves.toList();
-    for (FileSystemEntity e in saveList.reversed) {
-      if (e.path.contains('_')) {
-        final slot = int.parse(e.path.substring(e.path.lastIndexOf('_') + 1));
-        final name = "$saveLoc/${saveName}_";
-        e.renameSync("$name${slot + 1}");
-        if (slot + 1 > MAX_SAVES) {
-          Directory("$name${slot + 1}").deleteSync(recursive: true);
-        }
-      }
-    }
-    final backupSaveDir = Directory("$saveLoc/${saveName}_2");
-    copyDirectory(saveDir, backupSaveDir);
+void backupAndRenameSaves({
+  required List<FileSystemEntity> saveContentDirs,
+  required Directory saveDir,
+  required String saveLoc,
+  required String saveName,
+  required ContainerInfo containerInfo,
+}) {
+  /*
+  get a list of all the dirs that have the name of the save and have a .autobackup file
+  TODO: need more robust way to handle parent/children association 
+  then rename both the .container-info display name and dir after incrementing .autobackup
+  finally create the most recent save by taking a backup of the original
+  this keeps the _2 save the most recent at all times and allows the oldest to pop off when MAX_SAVES is hit
+  */
+  final likeSaves = saveContentDirs.where((e) => e.path.contains(saveName));
+  final existingSaves = <Directory>[];
+  for (FileSystemEntity v in likeSaves) {
+    v as Directory;
+    isAuto(v) ? existingSaves.add(v) : {};
   }
+  List<Directory> saveList = existingSaves.toList();
+  saveList.sort((a, b) {
+    final fileA = getContainerInfo(a).lastModifiedSync();
+    final fileB = getContainerInfo(b).lastModifiedSync();
+    return fileA.compareTo(fileB);
+  });
+  if (saveList.length > MAX_SAVES) saveList.first.deleteSync();
+  for (FileSystemEntity e in saveList) {
+    final dir = Directory(e.path);
+    final slot = SaveMeta.fromDirectory(dir).slot;
+    final meta = SaveMeta(slot: slot, parent: saveDir.path);
+    final container = ContainerInfo.fromDirectory(dir);
+    final name = container.value.containerMeta.displayName;
+    final newName = '${name.substring(0, name.lastIndexOf('_'))}_$slot';
+    final dst = Directory("$saveLoc/$newName");
+    copyDirectory(src: dir, dst: dst, meta: meta);
+    modifyContainerInfo(dst, container, '$newName');
+  }
+  final backupSaveDir = Directory("$saveLoc/${saveName}_2");
+  backupSaveDir.createSync(recursive: true);
+  final meta = SaveMeta(slot: 2, parent: saveDir.path);
+  copyDirectory(
+    src: saveDir,
+    dst: backupSaveDir,
+    meta: meta,
+  ); // starting slot at 2 means I don't have to parse display names
+  modifyContainerInfo(backupSaveDir, containerInfo, '${saveName}_2');
+}
+
+void modifyContainerInfo(Directory dir, ContainerInfo container, String name) {
+  sleep(.new(milliseconds: 200));
+  container.value.containerMeta.displayName = name;
+  final file = File('${dir.path}/.container-info');
+  JsonEncoder encoder = .withIndent('  ');
+  final prettyPrint = encoder.convert(container.toJson());
+  file.writeAsStringSync(prettyPrint, mode: .write, flush: true);
 }
 
 void restartWatcher(StreamSubscription? sub) {
@@ -151,24 +232,6 @@ void restartWatcher(StreamSubscription? sub) {
   Future.delayed(Duration(seconds: 1), startWatcher);
 }
 
-// void getContainerInfos(Directory appDataDir) {
-//   final appDataDirs = appDataDir.listSync().whereType<Directory>();
-//   List<File> containerInfos = <File>[];
-//   for (Directory dir in appDataDirs) {
-//     final dirList = dir.listSync();
-//     final files = dirList.whereType<File>();
-//     final backups = dirList.where((e) => e.path.contains('.backups'));
-//     if (backups.length > 0) {
-//       final backupsDirs = (backups as Directory)
-//           .listSync()
-//           .whereType<Directory>();
-//       for (Directory backDir in backupsDirs) {
-//         final backupFiles = backDir.listSync().whereType<File>();
-//       }
-//     }
-//   }
-// }
-//
 void startWatcher() {
   final appDataDir = Platform.environment['APPDATA'];
   final saveLoc = "$appDataDir/SpaceEngineers2/AppData/SaveGames";
@@ -183,37 +246,33 @@ void startWatcher() {
       final newSaveRawPath = event.path;
       if (newSaveRawPath.contains('.container-info')) {
         final containerInfo = File(newSaveRawPath);
-        // print(containterInfo.lastModifiedSync());
-        sleep(.new(milliseconds: 200));
+        sleep(.new(milliseconds: 200)); // wait for the game to finish writing
         final containerInfoContents = containerInfo.readAsStringSync();
         final containerJson = jsonDecode(containerInfoContents);
         assert(containerJson is Map);
         final container = ContainerInfo.fromJson(containerJson);
-        print(container.value.containerMeta.displayName);
+
+        final newSavePath = newSaveRawPath.substring(
+          0,
+          newSaveRawPath.lastIndexOf('\\'),
+        );
+        final newSaveDir = Directory(newSavePath);
+        final isBackup = isAuto(newSaveDir);
+        if (!isBackup) {
+          backupAndRenameSaves(
+            saveContentDirs: saveContentDirs,
+            saveDir: newSaveDir,
+            saveLoc: saveLoc,
+            saveName: container.value.containerMeta.displayName,
+            containerInfo: container,
+          );
+        }
         restartWatcher(sub);
       }
-      final newSavePath = newSaveRawPath.substring(
-        0,
-        newSaveRawPath.lastIndexOf('\\'),
-      );
-      final newSaveDir = Directory(newSavePath);
-      final newSaveName = newSavePath.substring(
-        newSavePath.lastIndexOf('\\') + 1,
-      );
-      final isBackup = isAuto(newSaveDir);
-      backupAndRenameSavesDirs(
-        saveContentDirs,
-        newSaveDir,
-        saveLoc,
-        newSaveName,
-        isBackup,
-      );
-      // restartWatcher(sub);
     }
   });
 }
 
 void main() async {
   startWatcher();
-  // Completer<void>().future;
 }
