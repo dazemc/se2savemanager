83a656eabd4a5bf631c3656df9e9080bd9ca2151
Daazed J McFarland <daazedjmcfarland@gmail.com>
2026-01-10 11:06:28 -0800

    should have just made a db, going to start redoing a bunch of stuff

--- lib/models.dart
+++ lib/models.dart
@@ -10,6 +10,47 @@ File getContainerInfo(Directory dir) {
   return cFile as File;
 }
 
+class SaveManager {
+  //TODO: Linux
+  final String installationDirectoryPath;
+  final String spaceEngineersSaveDirectoryPath;
+  SaveManager()
+    : installationDirectoryPath = Platform.isWindows
+          ? '${Platform.environment["APPDATA"]!}/se2savemanager'
+          : '',
+      spaceEngineersSaveDirectoryPath = Platform.isWindows
+          ? '${Platform.environment["APPDATA"]!}/SpaceEngineers2/AppData/SaveGames'
+          : '';
+
+  void install() async {
+    if (installationDirectoryPath.isNotEmpty) {
+      final dir = Directory(installationDirectoryPath);
+      if (await dir.exists()) {
+        print('se2savemanger already installed');
+        //TODO: give option to uninstall/reinstall
+        return;
+      }
+      await dir.create();
+      if (!await dir.exists()) {
+        throw 'Could not create se2savemanager directory at location: $dir';
+      }
+    }
+  }
+}
+
+class Save {
+  final SaveMeta saveMeta;
+  final ContainerInfo container;
+  final Directory location;
+  final Iterable<FileSystemEntity> data; //TODO: SaveData;
+  const Save({
+    required this.saveMeta,
+    required this.container,
+    required this.location,
+    required this.data,
+  });
+}
+
 class SaveMeta {
   final int slot;
   final Directory parent;
