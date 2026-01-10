3d9f5baff9a03284efdb2f61713970676495b65c
Daazed J McFarland <daazedjmcfarland@gmail.com>
2026-01-10 09:16:53 -0800

    pull out models

---
+++ lib/models.dart
@@ -0,0 +1,145 @@
+import 'dart:io';
+import 'dart:convert';
+
+// const int MAX_SAVES = 2;
+
+File getContainerInfo(Directory dir) {
+  final cFile = dir.listSync().firstWhere(
+    (e) => e.path.contains('.container-info'),
+  );
+  return cFile as File;
+}
+
+class SaveMeta {
+  final int slot;
+  final Directory parent;
+  const SaveMeta({required this.slot, required this.parent});
+
+  factory SaveMeta.fromJson(json) {
+    return SaveMeta(slot: json['slot'], parent: Directory(json['parent']));
+  }
+
+  factory SaveMeta.fromDirectory(Directory dir) {
+    final metaFile =
+        (dir.listSync().firstWhere((e) => e.path.contains('.autobackup'))
+                as File)
+            .readAsStringSync();
+    return SaveMeta.fromJson(jsonDecode(metaFile));
+  }
+
+  Map<String, dynamic> toJson() {
+    return {"slot": slot, "parent": parent.path};
+  }
+}
+
+class ContainerInfo {
+  final ContainerBundles bundles;
+  final String type;
+  final ContainerValue value;
+  const ContainerInfo({
+    required this.bundles,
+    required this.type,
+    required this.value,
+  });
+
+  factory ContainerInfo.fromJson(Map<String, dynamic> json) {
+    return ContainerInfo(
+      type: json[r'$Type'] as String,
+      bundles: ContainerBundles.fromJson(json[r'$Bundles']),
+      value: ContainerValue.fromJson(json[r'$Value']),
+    );
+  }
+
+  factory ContainerInfo.fromDirectory(Directory dir) {
+    final json = jsonDecode(getContainerInfo(dir).readAsStringSync());
+    return ContainerInfo.fromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => {
+    r'$Bundles': bundles.toJson(),
+    r'$Type': type,
+    r'$Value': value.toJson(),
+  };
+}
+
+class ContainerBundles {
+  final String systemRuntime;
+  final String vRage;
+  const ContainerBundles({required this.systemRuntime, required this.vRage});
+
+  factory ContainerBundles.fromJson(Map<String, dynamic> json) {
+    return ContainerBundles(
+      systemRuntime: json['System.Runtime'] as String,
+      vRage: json['VRage'] as String,
+    );
+  }
+  Map<String, dynamic> toJson() => {
+    'System.Runtime': systemRuntime,
+    'VRage': vRage,
+  };
+}
+
+class ContainerValue {
+  final ContainerMeta containerMeta;
+  final Map additonalData;
+  const ContainerValue({
+    required this.containerMeta,
+    required this.additonalData,
+  });
+
+  factory ContainerValue.fromJson(Map<String, dynamic> json) {
+    return ContainerValue(
+      additonalData: {}, // TODO: currently empty in my game
+      containerMeta: ContainerMeta.fromJson(json['Meta']),
+    );
+  }
+  Map<String, dynamic> toJson() => {
+    'Meta': containerMeta.toJson(),
+    'AdditionalData': additonalData,
+  };
+}
+
+class ContainerMeta {
+  final String? baseMetadata;
+  String displayName;
+  final String description;
+  final int gameVersion;
+  final int gameBuildNumber;
+  final int saveCreationTimeInTicks;
+  final int pcu;
+  final bool usedDebugMenu;
+  ContainerMeta({
+    this.baseMetadata,
+    required this.displayName,
+    required this.description,
+    required this.gameVersion,
+    required this.gameBuildNumber,
+    required this.saveCreationTimeInTicks,
+    required this.pcu,
+    required this.usedDebugMenu,
+  });
+
+  factory ContainerMeta.fromJson(Map<String, dynamic> json) {
+    return ContainerMeta(
+      baseMetadata: json['BaseMetadata'] as String?,
+      displayName: json['DisplayName'] as String,
+      description: json['Description'] as String,
+      gameVersion: json['GameVersion'] as int,
+      gameBuildNumber: json['GameBuildNumber'] as int,
+      saveCreationTimeInTicks: json['SaveGameCreationTimeInTicks'] as int,
+      pcu: json['PCU'] as int,
+      usedDebugMenu: json['UsedDebugMenu'] as bool,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'BaseMetadata': baseMetadata,
+    'DisplayName': displayName,
+    'Description': description,
+    "GameVersion": gameVersion,
+    "GameBuildNumber": gameBuildNumber,
+    "SaveGameCreationTimeInTicks": saveCreationTimeInTicks,
+    "PCU": pcu,
+    "UsedDebugMenu": usedDebugMenu,
+  };
+}
--- lib/watcher.dart
+++ lib/watcher.dart
@@ -1,143 +1,10 @@
 import 'package:watcher/watcher.dart';
+import 'models.dart';
 import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
 
 const int MAX_SAVES = 2;
-
-class SaveMeta {
-  final int slot;
-  final Directory parent;
-  const SaveMeta({required this.slot, required this.parent});
-
-  factory SaveMeta.fromJson(json) {
-    return SaveMeta(slot: json['slot'], parent: Directory(json['parent']));
-  }
-
-  factory SaveMeta.fromDirectory(Directory dir) {
-    final metaFile =
-        (dir.listSync().firstWhere((e) => e.path.contains('.autobackup'))
-                as File)
-            .readAsStringSync();
-    return SaveMeta.fromJson(jsonDecode(metaFile));
-  }
-
-  Map<String, dynamic> toJson() {
-    return {"slot": slot, "parent": parent.path};
-  }
-}
-
-class ContainerInfo {
-  final ContainerBundles bundles;
-  final String type;
-  final ContainerValue value;
-  const ContainerInfo({
-    required this.bundles,
-    required this.type,
-    required this.value,
-  });
-
-  factory ContainerInfo.fromJson(Map<String, dynamic> json) {
-    return ContainerInfo(
-      type: json[r'$Type'] as String,
-      bundles: ContainerBundles.fromJson(json[r'$Bundles']),
-      value: ContainerValue.fromJson(json[r'$Value']),
-    );
-  }
-
-  factory ContainerInfo.fromDirectory(Directory dir) {
-    final json = jsonDecode(getContainerInfo(dir).readAsStringSync());
-    return ContainerInfo.fromJson(json);
-  }
-
-  Map<String, dynamic> toJson() => {
-    r'$Bundles': bundles.toJson(),
-    r'$Type': type,
-    r'$Value': value.toJson(),
-  };
-}
-
-class ContainerBundles {
-  final String systemRuntime;
-  final String vRage;
-  const ContainerBundles({required this.systemRuntime, required this.vRage});
-
-  factory ContainerBundles.fromJson(Map<String, dynamic> json) {
-    return ContainerBundles(
-      systemRuntime: json['System.Runtime'] as String,
-      vRage: json['VRage'] as String,
-    );
-  }
-  Map<String, dynamic> toJson() => {
-    'System.Runtime': systemRuntime,
-    'VRage': vRage,
-  };
-}
-
-class ContainerValue {
-  final ContainerMeta containerMeta;
-  final Map additonalData;
-  const ContainerValue({
-    required this.containerMeta,
-    required this.additonalData,
-  });
-
-  factory ContainerValue.fromJson(Map<String, dynamic> json) {
-    return ContainerValue(
-      additonalData: {}, // TODO: currently empty in my game
-      containerMeta: ContainerMeta.fromJson(json['Meta']),
-    );
-  }
-  Map<String, dynamic> toJson() => {
-    'Meta': containerMeta.toJson(),
-    'AdditionalData': additonalData,
-  };
-}
-
-class ContainerMeta {
-  final String? baseMetadata;
-  String displayName;
-  final String description;
-  final int gameVersion;
-  final int gameBuildNumber;
-  final int saveCreationTimeInTicks;
-  final int pcu;
-  final bool usedDebugMenu;
-  ContainerMeta({
-    this.baseMetadata,
-    required this.displayName,
-    required this.description,
-    required this.gameVersion,
-    required this.gameBuildNumber,
-    required this.saveCreationTimeInTicks,
-    required this.pcu,
-    required this.usedDebugMenu,
-  });
-
-  factory ContainerMeta.fromJson(Map<String, dynamic> json) {
-    return ContainerMeta(
-      baseMetadata: json['BaseMetadata'] as String?,
-      displayName: json['DisplayName'] as String,
-      description: json['Description'] as String,
-      gameVersion: json['GameVersion'] as int,
-      gameBuildNumber: json['GameBuildNumber'] as int,
-      saveCreationTimeInTicks: json['SaveGameCreationTimeInTicks'] as int,
-      pcu: json['PCU'] as int,
-      usedDebugMenu: json['UsedDebugMenu'] as bool,
-    );
-  }
-
-  Map<String, dynamic> toJson() => {
-    'BaseMetadata': baseMetadata,
-    'DisplayName': displayName,
-    'Description': description,
-    "GameVersion": gameVersion,
-    "GameBuildNumber": gameBuildNumber,
-    "SaveGameCreationTimeInTicks": saveCreationTimeInTicks,
-    "PCU": pcu,
-    "UsedDebugMenu": usedDebugMenu,
-  };
-}
 
 void copyDirectory({
   bool createMetaData = true,
@@ -184,13 +51,6 @@ bool isIgnored(Directory saveDir) {
   return saveDir.listSync().any((c) => c.path.contains('.tmp'));
 }
 
-File getContainerInfo(Directory dir) {
-  final cFile = dir.listSync().firstWhere(
-    (e) => e.path.contains('.container-info'),
-  );
-  return cFile as File;
-}
-
 Iterable<FileSystemEntity> getLikeSaves(
   //TODO:
   Directory rootSaveDir,
@@ -230,7 +90,9 @@ void backupAndRenameSaves({
     final fileB = getContainerInfo(b).lastModifiedSync();
     return fileA.compareTo(fileB);
   });
+  print(existingSaves.length);
   if (existingSaves.length >= MAX_SAVES) {
+    existingSaves.forEach(print);
     final lastSave = existingSaves.last;
     print("Deleting oldest save: ${lastSave.path}");
     lastSave.deleteSync(recursive: true);
@@ -240,7 +102,7 @@ void backupAndRenameSaves({
   for (FileSystemEntity e in existingSaves) {
     final dir = Directory(e.path);
     final slot = SaveMeta.fromDirectory(dir).slot + 1;
-    final meta = SaveMeta(slot: slot, parent: saveDir);
+    SaveMeta meta = SaveMeta(slot: slot, parent: saveDir);
     ContainerInfo container = ContainerInfo.fromDirectory(dir);
     String name = container.value.containerMeta.displayName;
     String newName = '${name.substring(0, name.lastIndexOf('_'))}_$slot';
@@ -249,15 +111,17 @@ void backupAndRenameSaves({
     Directory parent = meta.parent;
     print(saveDir.path);
     print(isBackup(saveDir));
-    if (meta.parent.existsSync() && isBackup(saveDir)) {
+    if (isBackup(saveDir) && meta.parent.existsSync()) {
+      meta = SaveMeta.fromDirectory(e as Directory);
       print("backup trying to save");
-      if (meta.parent.path.contains('.backups')) {
+      if (meta.parent.parent.path.contains('.backups')) {
+        print("save is coming from .backups dir");
         parent = SaveMeta.fromDirectory(
           meta.parent.parent.parent,
         ).parent; // TODO: add meta trueParent property
-        container = ContainerInfo.fromDirectory(meta.parent.parent.parent);
-        print("Container location: ${meta.parent.parent.parent}");
-        newName = container.value.containerMeta.displayName;
+        newName = ContainerInfo.fromDirectory(
+          meta.parent,
+        ).value.containerMeta.displayName;
       }
       final containerParent = ContainerInfo.fromDirectory(dst);
       dst = parent;
