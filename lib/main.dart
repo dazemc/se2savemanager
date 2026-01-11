4be6f511543b797fe6f7cfd041bee2b9c08f0714
Daazed J McFarland <daazedjmcfarland@gmail.com>
2026-01-11 12:14:15 -0800

    init savemanger with watcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:hive_ce/hive_ce.dart';
-import 'package:se2savemanager/models/save_manager.dart';
-import 'package:se2savemanager/services/save_watcher.dart';
+import 'package:se2savemanager/services/save_manager.dart';
 
-void main() async {
-  Hive.init('se2savemanager');
-  final Box box = await Hive.openBox('.');
-  box.put('name', 'test');
-  final dynamic name = box.get('name');
+Future<void> main() async {
+  final saveManager = SaveManager();
+  await saveManager.init();
+  saveManager.box.put('name', 'test');
+  final dynamic name = saveManager.box.get('name');
   assert(name is String);
   print(name);
-  final saveManger = SaveManager();
-  final watcher = SaveWatcher(
-    watchPath: saveManger.spaceEngineersSaveDirectoryPath,
-    onChange: (path) => print(path),
-  ).start();
+  saveManager.watcher.start();
   print('Non-blocking test');
 }
--- lib/services/save_manager.dart
+++ lib/services/save_manager.dart
@@ -1,4 +1,7 @@
 import 'dart:io';
+import 'package:hive_ce/hive_ce.dart';
+import 'package:se2savemanager/services/save_watcher.dart';
+import 'package:watcher/watcher.dart';
 
 class SaveManager {
   //TODO: Linux
@@ -12,18 +15,32 @@ class SaveManager {
           ? '${Platform.environment["APPDATA"]!}/SpaceEngineers2/AppData/SaveGames'
           : '';
 
-  void install() async {
+  late final Box box;
+  late final SaveWatcher watcher;
+
+  Future<void> _install() async {
     if (installationDirectoryPath.isNotEmpty) {
       final dir = Directory(installationDirectoryPath);
       if (await dir.exists()) {
         print('se2savemanger already installed');
         //TODO: give option to uninstall/reinstall
         return;
+      } else {
+        await dir.create();
       }
-      await dir.create();
       if (!await dir.exists()) {
         throw 'Could not create se2savemanager directory at location: $dir';
       }
     }
+  }
+
+  Future<void> init() async {
+    await _install();
+    Hive.init(installationDirectoryPath);
+    box = await Hive.openBox('se2savemanager');
+    watcher = SaveWatcher(
+      watchPath: spaceEngineersSaveDirectoryPath,
+      onChange: (path) => print(path),
+    );
   }
 }
