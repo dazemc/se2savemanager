284b845e05a35c2c4c468842c991d99830053e93
Daazed McFarland <daazedjmcfarland@gmail.com>
2026-01-13 11:35:38 -0800

    gui/bloc init

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,39 @@
+import 'package:fluent_ui/fluent_ui.dart';
 import 'dart:io';
 
 import 'package:se2savemanager/services/save_logger.dart';
 import 'package:se2savemanager/services/save_manager.dart';
+import 'package:system_theme/system_theme.dart';
 
-Future<void> main() async {
-  final log = SaveLogger(name: pid.toString()).log;
-  final saveManager = SaveManager();
-  await saveManager.init();
-  saveManager.box.put('name', 'test');
-  final dynamic name = saveManager.box.get('name');
-  assert(name is String);
-  log.info(name);
-  saveManager.watcher.start();
-  log.info('Non-blocking test');
+// Future<void> main() async {
+//   final log = SaveLogger(name: pid.toString()).log;
+//   final saveManager = SaveManager();
+//   await saveManager.init();
+//   saveManager.box.put('name', 'test');
+//   final dynamic name = saveManager.box.get('name');
+//   assert(name is String);
+//   log.info(name);
+//   saveManager.watcher.start();
+//   log.info('Non-blocking test');
+// }
+
+void main() => runApp(const SaveApp());
+
+class SaveApp extends StatelessWidget {
+  const SaveApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FluentApp(
+      title: "Space Engineers 2 Save Manager",
+      theme: .new(
+        brightness: .dark,
+        accentColor: SystemTheme.accentColor.accent.toAccentColor(),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: ScaffoldPage(
+        content: Row(mainAxisAlignment: .center, children: [Text('test')]),
+      ),
+    );
+  }
 }
